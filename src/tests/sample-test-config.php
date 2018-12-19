fe459417a9b359cdae33ad28b3d4e7f54b7b8c84
Matt <mattlam@uw.edu>
2018-12-19 15:54:37 -0800

    Add sample test config file.

---
+++ src/tests/sample-test-config.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Config file for unit testing
+ *
+ * Make a copy of this in the current directory, name 'test-config.php', uncomment lines, and replace bracketed values
+ * with correct values for this environment.
+ */
+
+/** -------------------------------------------------------------------------
+ *  Database Connection
+ ** -------------------------------------------------------------------------*/
+// define('TEST_DB_NAME', '[test database name]');
+// define('TEST_DB_HOST', '[test database host IP or domain name]');
+// define('TEST_DB_PORT', [test database port #]);
+// define('TEST_DB_USER', '[test database user name]');
+// define('TEST_DB_PASS', '[test database user password]');
+
+/** -------------------------------------------------------------------------
+ *  Mock User Data
+ ** -------------------------------------------------------------------------*/
+// define('MOCK_ID', 1);
